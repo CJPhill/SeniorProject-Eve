35c85c89defe4e39fdbfd2e5bdaa7c61c7230a67
Julia von Goetz <jvongoetz808@gmail.com>
2024-11-11 18:09:45 -0800

    travel

---
+++ EVE/Assets/Scripts/LoadIsland.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Cinemachine.DocumentationSortingAttribute;
+using UnityEngine.SceneManagement;
+
+public class LoadIsland : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenIsland1()
+    {
+        SceneManager.LoadScene("Test");
+    }
+}
