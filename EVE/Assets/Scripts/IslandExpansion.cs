d498a7a17022f9a09c70fc1b01fe0641de3709b6
CJPhill <cphill39@lion.lmu.edu>
2024-11-24 23:41:57 -0800

    added sprite and rotation for island expansion

--- EVE/Assets/Scripts/IslandExpansion.cs
+++ EVE/Assets/Scripts/IslandExpansion.cs
@@ -8,7 +8,34 @@ public class IslandExpansion : MonoBehaviour, IInteractable
     public bool built = false; //Maybe not needed
     public bool hasMoney = true; //Will need to be changed to false when able to check
     public GameObject bridge;
+    public GameObject camera;
+    private Transform cameraTransform;
+    public SpriteRenderer rbSprite;
 
+    private void Start()
+    {
+        cameraTransform = camera.transform;
+    }
+
+    private void Update()
+    {
+        FaceCamera();
+    }
+
+    private void FaceCamera()
+    {
+        // Get the direction from the sprite to the camera
+        Vector3 directionToCamera = cameraTransform.position - rbSprite.transform.position;
+
+        // Set the y-component to zero to keep the rotation on the horizontal plane
+        directionToCamera.y = 0;
+
+        // Rotate the sprite to face the camera
+        if (directionToCamera != Vector3.zero)
+        {
+            rbSprite.transform.rotation = Quaternion.LookRotation(-directionToCamera);
+        }
+    }
 
     public void receiveInteract()
     {
