5fadf80f597e32e41b7a47df1a524aa118c16697
Matthew Lee <mlee164@lion.lmu.edu>
2025-04-12 16:25:11 -0700

    Transitions Implemented

---
+++ EVE/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour, IInteractable
+{
+    [SerializeField] RectTransform fader;
+    [SerializeField] string sceneToLoad;
+
+    private void Start(){
+        fader.gameObject.SetActive(true);
+
+        LeanTween.scale(fader, new Vector3(1,1,1), 0);
+        LeanTween.scale(fader, Vector3.zero, 0.5f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() =>
+        {
+            fader.gameObject.SetActive(false);
+        });
+    }
+
+    public void receiveInteract()
+    {
+        fader.gameObject.SetActive(true);
+        LeanTween.scale(fader, Vector3.zero, 0f);
+        LeanTween.scale(fader, new Vector3(1,1,1), 0.5f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() =>
+        {
+            Invoke("LoadScene", 0.5f);
+
+    });
+    }
+
+    private void LoadScene(){
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogWarning("SceneHandler: targetSceneName is not set!");
+        }
+    }
+}
